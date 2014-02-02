b81acd37302e342623ebdc8eb7f6956ad265c24c
ruellebe <devnull@localhost>
2014-02-02 23:10:57 +0100

    less common replacement for illegal caracters

--- src/main/java/com/googlecode/n_orm/mongo/MongoNameSanitizer.java
+++ src/main/java/com/googlecode/n_orm/mongo/MongoNameSanitizer.java
@@ -8,8 +8,8 @@ public class MongoNameSanitizer {
 	public static String sanitize(String name) {
 		String sanitizedName = name;
 		if (name != null) {
-			sanitizedName = sanitizedName.replace("$", "_Dollar_");
-			sanitizedName = sanitizedName.replace(".", "_Dot_");
+			sanitizedName = sanitizedName.replace("$", "_SsJu2_");
+			sanitizedName = sanitizedName.replace(".", "_NtVrM_");
 		}
 		return sanitizedName;
 	}
@@ -17,8 +17,8 @@ public class MongoNameSanitizer {
 	public static String dirty(String name) {
 		String dirtiedName = name;
 		if (name != null) {
-			dirtiedName = dirtiedName.replace("_Dollar_", "$");
-			dirtiedName = dirtiedName.replace("_Dot_", ".");
+			dirtiedName = dirtiedName.replace("_SsJu2_", "$");
+			dirtiedName = dirtiedName.replace("_NtVrM_", ".");
 		}
 		return dirtiedName;
 	}
